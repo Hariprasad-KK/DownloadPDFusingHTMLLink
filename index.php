6a111d89f74b3bfe249d29a1b971837f78648aac
Hariprasad K K <56286476+Hariprasad-KK@users.noreply.github.com>
2021-08-28 07:14:14 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Download PDF using PHP from HTML link</title>
+</head>
+<body>
+    <center>
+        <h2 style="color:#d7385e;">Welcome to VIT-AP</h2>
+        <p>Click below to Download PDF</p>
+        <a href="Coursera.pdf" target="_VITAP">Click on this to view Certificate</a>
+    </center>
+</body>
+</html>
---
+++ indexback.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <center>
+        <h2 style="color:#d7385e;">Welcome to VIT-AP</h2>
+        <p>Click below to Download PDF</p>
+        <a href="download.php?file=Coursera" target="_VITAP">Click on this to download Certificate</a>
+    </center>
+</body>
+</html>
